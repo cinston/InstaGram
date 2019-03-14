cee15fd7597b9dce0bd167ff0107d781d8276eff
cinston <wagawc@gmail.com>
2019-03-14 16:39:07 +0300

    updated urls

--- pictures/urls.py
+++ pictures/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 
 urlpatterns = [
-    # url(r'^$', views.insta_index),
     url(r'^home/$',views.home_index, name="homePage"),
     url(r'^$', views.signup),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'comments/',views.save_comment, name='comment'),
-    url(r'profile/',views.profile_index, name='profile')
+    url(r'profile/',views.profile_index, name='profile'),
+    url(r'update/',views.update_index, name='update')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
