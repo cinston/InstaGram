9a987c0641e30cff95a4e46e4a9f545cfcf54c6c
cinston <wagawc@gmail.com>
2019-03-14 12:43:29 +0300

    updated models,views and templates

--- instagram/settings.py
+++ instagram/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODE=config("MODE", default="dev")
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*0^#@5s2+6c12j=zv3y7dl#42$7k8a8r92a-k5a)wh-v)rzqw+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pictures',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'instagram.urls'
@@ -62,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -76,9 +85,35 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'instagram',
+        'USER': 'carlos',
+    'PASSWORD':'zagadat',
     }
 }
+
+if config('MODE')=="dev":
+	DATABASES = {
+		'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': config('DB_NAME'),
+		'USER': config('DB_USER'),
+		'PASSWORD': config('DB_PASSWORD'),
+		'HOST': config('DB_HOST'),
+		'PORT': '',
+		}
+
+	}
+		# production
+else:
+	DATABASES = {
+		'default': dj_database_url.config(
+		default=config('DATABASE_URL')
+		)
+		}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Password validation
@@ -105,7 +140,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -117,4 +152,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
+LOGIN_REDIRECT_URL='homePage'
+# django_heroku.settings(locals())
+LOGIN_REDIRECT_URL='homePage'
--- pictures/forms.py
+++ pictures/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-from .models import image
+from .models import *
 
-UploadForm(forms.ModelForms):
-class Meta:
-    model = Image 
-    field = ('pic','desctiption')
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('comment',)
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('first_name',)
+
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields =['image_path']
